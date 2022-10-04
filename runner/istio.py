a87a241b5b3276bedfc4685a872143879f5cb1a6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 18:24:52 +0000

    Adding numeric_owner as keyword arguement

--- runner/istio.py
+++ runner/istio.py
@@ -127,7 +127,7 @@ def _extract(archive_path: str, extracted_dir_path: str) -> str:
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tar, path=extracted_dir_path)
